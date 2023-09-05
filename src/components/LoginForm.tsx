c2bb3f38a016e185e2b643a692f19fbdafc1dcf4
ledei <holybuzz@live.se>
2023-09-05 13:10:11 +0200

    made a handeler for user credentials

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import { useState } from "react";
+
+interface userDetails {
+  username: string;
+  password: string;
+}
 
 export const LoginForm: React.FC = () => {
+  const [credential, setCredential] = useState<userDetails>({ username: "", password: "" });
+
+  const handleCredential = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredential({ ...credential, [e.target.name]: e.target.value });
+  };
   return (
     <form>
       <div>
         <label htmlFor="">Användarnamn</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => {
+            handleCredential(e);
+          }}
+        />
       </div>
       <div>
         <label htmlFor="">Lössenord</label>
-        <input type="password" />
+        <input
+          type="password"
+          onChange={(e) => {
+            handleCredential(e);
+          }}
+        />
       </div>
 
       <button type="submit">Login</button>
