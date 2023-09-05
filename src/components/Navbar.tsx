adc7a26a9be3ef3d720daeb690d906a002b59d2f
ledei <holybuzz@live.se>
2023-09-05 14:36:10 +0200

    working on handleLogout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,19 @@ export const Navbar: React.FC = () => {
   const handleToggle = () => {
     setToggle((toggle) => !toggle);
   };
+
+  const handleLogout = () => {
+    setToggle(false);
+    setUsername("");
+  };
   return (
     <div>
       <Link to="booking"> Våra pass</Link>
       <Link to="/"> Hem</Link>
       <p>Strong n' Epic</p>
       {username !== "" && <p>inloggad som {username}</p>}
-      <button onClick={handleToggle}>Logga in</button>
+      {username === "" ? <button onClick={handleToggle}>Logga in</button> : <button onClick={handleLogout}>Logga ut</button>}
+
       {toggle && <LoginForm setUsername={setUsername} handleToggle={handleToggle} />}
     </div>
   );
