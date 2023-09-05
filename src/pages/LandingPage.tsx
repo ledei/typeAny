f04a66697d7113b037f9ef6ef184f4b6a2a9c7bd
Niklas nguyen <113009525+ledei@users.noreply.github.com>
2023-09-05 10:23:24 +0200

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ const LandingPage = (): JSX.Element => {
   return (
     <div>
       <div>
-        <h1>Title</h1>
+        <h1>Strong n' Epic</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
           magnam nemo repellat, adipisci nihil velit aliquid. Quisquam beatae
