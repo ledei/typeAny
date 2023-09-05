89e52f495b8ac6efe7d1331adb414a38cf49d5dd
Nemkunde <113009899+Nemkunde@users.noreply.github.com>
2023-09-05 10:36:06 +0200

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,6 +10,16 @@ const LandingPage = (): JSX.Element => {
           reiciendis illum sunt!
         </p>
       </div>
+      <div>
+      <h2>Våra Pass</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
+        magnam nemo repellat, adipisci nihil velit aliquid. Quisquam beatae
+        aliquam ratione quas, doloribus, molestias delectus nam assumenda qui
+        reiciendis illum sunt!
+      </p>
+      <button>Se här</button>
+      </div>
     </div>
   );
 };
